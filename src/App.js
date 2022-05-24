275a334e2a43410b7c33bdcf71965f6db5f783a7
Ashwini Bhoskar <ashwinibhoskar5111@gmail.com>
2022-05-24 19:44:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import Todos from "./components/Todos";
+
+// import React, { useState, useEffect } from "react";
+// let A =() =>  <div  onClick={()=>
+//    setCount(count+1)}>Components A</div>;
+// let B =() =><div>Components B</div>;
 
 function App() {
+  // const [count1, setCount1] = useState(0);
+  // const [count2, setCount2] = useState(0);
+
+  // fetch ("https://m6g3bt.sse.codesandbox.io/")
+  //   .then ((r)=>r.json())
+  //   .then((d)=>{
+  //     console.log(d);
+  //   });
+  // useEffect(() => {
+  //  console.log(2);
+  // },[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div className="App">
+      {/* <div>
+      <div className="App" onClick={()=> setCount1(count1+1)}>Counter1:{count1}</div>
     </div>
+
+    <div>
+      <div className="App" onClick={()=> setCount2(count2+1)}>Counter1:{count2}</div>
+    </div> */}
+    {/* <div className="App">App</div> */}
+    <Todos />
+ </div>
   );
 }
 
---
+++ src/components/Todos.jsx
@@ -0,0 +1,55 @@
+ import React, { useState, useEffect } from "react";
+ import styles from './Todos.module.css'
+
+const Todos = () => {
+    const [todos,setTodos] =useState([]);
+    const [newTodo,setNewTodo]=useState("")
+
+    const saveInfo =()=>{
+        fetch ("http://localhost:8080/todos",{
+        method :"POST",
+        headers:{
+            "content-type":"application/json",
+        },
+        body:JSON.stringify({
+            title:newTodo,
+            isCompleted:false,
+        }),
+    })
+        .then((r)=>r.json())
+        .then((d)=>{
+            setNewTodo([...todos,d]);
+            setNewTodo("");
+        });
+    };
+
+    useEffect (()=>{
+        fetch("http://localhost:8080/todos?_page=1&_limit=1")
+        .then((r)=>r.json())
+        .then((d)=>{
+            setTodos(d);
+        })
+    },[])
+  return (
+    <div>Todos
+       <div>
+           <input 
+           value ={newTodo}
+           onChange={({target})=>setNewTodo(target.value)}
+           />
+           <button onClick={saveInfo}>+</button>
+       </div>
+       
+        <div>
+        {todos.map((todo)=>(
+            <ul className={styles.tablehead}>
+                <li key={todo.id}>{todo.title}</li>
+            </ul>
+            
+        ))}
+        </div>
+    </div>
+  )
+}
+
+export default Todos;
